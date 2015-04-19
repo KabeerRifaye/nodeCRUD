7f5be5ad33b4b5bbb5a0127d21032fd157fb59e7
KabeerAdappt <kabeer.r@seocompany.com>
2015-04-19 07:59:01 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+
+/* Module Dependencies */
+
+// var mongoose = require('mongoose');
+
+app.listen(process.env.PORT || 3000 , function(){
+	console.log("Express server Listening on port "+(process.env.PORT || 3000));
+})
