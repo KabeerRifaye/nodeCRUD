f9fa5ef4a0fc4a8b1a53cb09dacd7bf5a79e1618
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-05-12 12:52:11 +0530

    mongoose fix

--- models/init.js
+++ models/init.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 
 var db = 'meanCRUD';
 
+var process.env.MONGOLAB_URI = 'mongodb://heroku_app36750892:vl4395v7dgsc650ev63oovn7m9@ds035702.mongolab.com:35702/heroku_app36750892';
+
 var URI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/'+db;
 
 /* Connect Mongoose */
