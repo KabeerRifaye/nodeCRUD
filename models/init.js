901a411250d6662e5f059d8d8b298d706360fdc8
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-05-13 16:16:25 +0530

    fix

--- models/init.js
+++ models/init.js
@@ -4,13 +4,11 @@ var mongoose = require('mongoose');
 
 var db = 'meanCRUD';
 
-var MONGOLAB_URI =  'mongodb://heroku_app36750892:vl4395v7dgsc650ev63oovn7m9@ds035702.mongolab.com:35702/heroku_app36750892';
+var MONGOLAB_URI =  'mongodb://heroku_app36750892:jksqplro0dos111c74irhae67j@ds063170.mongolab.com:63170/heroku_app36750892';
 
 var MONGOHQ_URL = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
 
-// var URI = 'mongodb://localhost/'+db || MONGOHQ_URL;
-var URI = MONGOHQ_URL || 'mongodb://localhost/'+db;
-// var URI = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
+var URI = MONGOLAB_URI || MONGOHQ_URL || 'mongodb://localhost/'+db;
 
 /* Connect Mongoose */
 
