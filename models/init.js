6778d28ca46b375f5425927eb893a1447f298312
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-05-13 15:12:36 +0530

    added mongoHQ

--- models/init.js
+++ models/init.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose');
 
 var db = 'meanCRUD';
 
-var process.env.MONGOLAB_URI = 'mongodb://heroku_app36750892:vl4395v7dgsc650ev63oovn7m9@ds035702.mongolab.com:35702/heroku_app36750892';
+var MONGOLAB_URI =  'mongodb://heroku_app36750892:vl4395v7dgsc650ev63oovn7m9@ds035702.mongolab.com:35702/heroku_app36750892';
 
-var URI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/'+db;
+var MONGOHQ_URL = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
+
+var URI = MONGOHQ_URL || 'mongodb://localhost/'+db;
+// var URI = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
 
 /* Connect Mongoose */
 
