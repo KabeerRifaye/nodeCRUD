a479eeaa047a6da231909977b10b613cf1f3c5ff
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-05-13 16:03:20 +0530

    fix

--- models/init.js
+++ models/init.js
@@ -8,6 +8,7 @@ var MONGOLAB_URI =  'mongodb://heroku_app36750892:vl4395v7dgsc650ev63oovn7m9@ds0
 
 var MONGOHQ_URL = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
 
+// var URI = 'mongodb://localhost/'+db || MONGOHQ_URL;
 var URI = MONGOHQ_URL || 'mongodb://localhost/'+db;
 // var URI = 'mongodb://heroku:jtXW_5x9qev3ohrpaSZtd-iezBvchlTJ-NF8HystrT88jiGag-7-gGlBJmX1mtl8jib7sJ4ZzZDiYQQNkftGjQ@candidate.10.mongolayer.com:10422,candidate.7.mongolayer.com:10403/app36750892';
 
